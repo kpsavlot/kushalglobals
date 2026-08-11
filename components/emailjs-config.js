20ec76eaf58c2652820c8e6c188d1afdedbcb18f
kpsavlot <ksanwlot5566@gmail.com>
2026-08-11 20:29:44 +0530

    Add EmailJS contact and quote forms

---
+++ components/emailjs-config.js
@@ -0,0 +1,11 @@
+window.EMAILJS_CONFIG = {
+    publicKey: 'A8seiCrIxcS1psXWY',
+    contact: {
+        serviceId: 'service_g9yg8w6',
+        templateId: 'template_xaumk3n'
+    },
+    quote: {
+        serviceId: 'service_g9yg8w6',
+        templateId: 'template_0czu2l5'
+    }
+};
